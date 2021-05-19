0242425b6612c76dc72d8367933fcc5e3cf20240
Victor Koschel <victor.andrade@venturus.org.br>
2021-05-19 11:12:06 -0300

    Tasks UI deletion

--- src/App.js
+++ src/App.js
@@ -24,12 +24,22 @@ function App() {
     }
 ])
 
+ const deleteTask = (id) => {
+   setTasks(tasks.filter((task) => task.id !== id))
+ }
+
   return (
     <div className="container">
       <Header  />
-      <Tasks tasks={tasks} />
+      {tasks.length> 0 ? (
+ <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        'No corres'
+      )}
+     
+      
     </div>
-  );
+  )
 }
 
 export default App;
