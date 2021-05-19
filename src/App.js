eb7308e68ca85f3d4977f87c6bb1882d63c90c3b
Victor Koschel <victor.andrade@venturus.org.br>
2021-05-19 10:42:41 -0300

    Tasks implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Consulta',
+        day: 'Feb 6th at 3:00',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Rolê',
+        day: 'Feb 7th at 3:00',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Reunião',
+        day: 'Feb 68h at 3:00',
+        reminder: false,
+    }
+])
+
   return (
     <div className="container">
       <Header  />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
 
 export default App;
+
