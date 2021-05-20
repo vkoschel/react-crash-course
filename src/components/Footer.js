fedee21ab7121b4e43a37ff83d0c2c46eeacfb6f
Victor Koschel <victor.andrade@venturus.org.br>
2021-05-20 08:41:41 -0300

    Victex icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
+import logo from './logo.png'
 
 const Footer = () => {
 
@@ -8,6 +9,7 @@ const Footer = () => {
         <footer>
             <p>Copyright &copy; 2021 </p>
             {location.pathname === '/' && <Link to="about">About</Link>}
+            <p><img src={logo} alt="Logo" /></p>
         </footer>
     )
 }
